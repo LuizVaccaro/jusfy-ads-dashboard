cb2ef4f28515fb95281e309f21764390e21c3adb
LuizVaccaro <vaccaroluiz@gmail.com>
2026-06-28 19:30:52 -0300

    chore: deploy.ps1 faz git push + netlify deploy juntos

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,15 @@
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 $env:NETLIFY_AUTH_TOKEN = [System.Environment]::GetEnvironmentVariable("NETLIFY_AUTH_TOKEN","User")
+
+# Git: commita e envia para o GitHub
+$status = git status --porcelain
+if ($status) {
+  $msg = Read-Host "Mensagem do commit (Enter = 'update')"
+  if (-not $msg) { $msg = "update" }
+  git add .
+  git commit -m $msg
+}
+git push
+
+# Netlify: publica em produção
 npx netlify-cli deploy --prod
